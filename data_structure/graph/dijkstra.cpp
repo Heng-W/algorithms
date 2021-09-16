70676952cce2fc901086d12e7d660736319cd453
Heng-W <heng_wei@163.com>
2021-09-16 23:41:37 +0800

    up

---
+++ data_structure/graph/dijkstra.cpp
@@ -0,0 +1,83 @@
+
+#include "graph_type.h"
+
+
+//Dijkstra算法
+template <class T>
+void dijkstra(const MGraph<T>& graph, int start,
+              std::vector<int>& pathArc,
+              std::vector<int>& shortPath)
+{
+    pathArc.resize(graph.vexNum(), -1);//前驱顶点下标
+    shortPath.resize(graph.vexNum());
+    std::vector<bool> final(graph.vexNum(), false);
+
+    for (int j = 0; j < graph.vexNum(); ++j)
+    {
+        shortPath[j] = graph.arcs[start][j];
+    }
+    shortPath[start] = 0;
+    final[start] = true;
+    //递推求取最短路径
+    for (int i = 1; i < graph.vexNum(); ++i)
+    {
+        int min = INT_MAX;
+        int k;
+        for (int j = 0; j < graph.vexNum(); ++j)
+        {
+            if (!final[j] && shortPath[j] < min)
+            {
+                k = j;
+                min = shortPath[j];
+            }
+        }
+        final[k] = true;
+        //修正当前的最短路径
+        for (int j = 0; j < graph.vexNum(); ++j)
+        {
+            if (!final[j] && graph.arcs[k][j] < INT_MAX &&
+                    min + graph.arcs[k][j] < shortPath[j])
+            {
+                shortPath[j] = min + graph.arcs[k][j];
+                pathArc[j] = k;
+            }
+        }
+    }
+}
+
+#include <cstdio>
+
+int main()
+{
+    using namespace std;
+    vector<int> vexs;
+    for (int i = 0; i < 9; ++i) vexs.push_back(i);
+    MGraph<int> graph(vexs,
+    {
+        {0, 1, 1}, {0, 2, 5}, {1, 2, 3}, {1, 3, 7}, {1, 4, 5},
+        {2, 4, 1}, {2, 5, 7}, {3, 4, 2}, {3, 6, 3}, {4, 5, 3},
+        {4, 6, 6}, {4, 7, 9}, {5, 7, 5}, {6, 7, 2}, {6, 8, 7},
+        {7, 8, 4}
+    });
+    int start = 2;
+    vector<int> pathArc, shortPath;
+    dijkstra(graph, start, pathArc, shortPath);
+
+    for (int i = 0; i < graph.vexNum(); ++i)
+    {
+        printf("v%d - v%d: ", start, i);
+        int j = i;
+        while (pathArc[j] != -1)
+        {
+            printf("%d ", pathArc[j]);
+            j = pathArc[j];
+        }
+        printf("\n");
+    }
+    printf("\nshortest path:\n");
+    for (int i = 0; i < graph.vexNum(); ++i)
+    {
+        printf("v%d - v%d: %d\n", graph.vexs[start], graph.vexs[i], shortPath[i]);
+    }
+    return 0;
+}
---
+++ data_structure/graph/topological_sort.cpp
@@ -0,0 +1,139 @@
+
+#include <vector>
+#include <stack>
+#include <iostream>
+
+
+//边表
+struct ArcNode
+{
+    int adjvex; //邻接序号
+    int weight = 1;
+    ArcNode* next;
+};
+
+//顶点表
+template <class Vertex>
+struct VertexNode
+{
+    using VertexType = Vertex;
+
+    VertexType data;
+    int in; //入度
+    ArcNode* firstArc;
+
+    VertexNode(const VertexType& _data)
+        : data(_data), in(0), firstArc(nullptr) {}
+
+    VertexNode(VertexType&& _data)
+        : data(std::move(_data)), in(0), firstArc(nullptr) {}
+};
+
+
+template <class Vertex>
+struct ALGraph
+{
+    using VertexType = Vertex;
+    using AdjList = std::vector<VertexNode<Vertex>>;
+    
+    struct Arc
+    {
+        int begin;
+        int end;
+        int weight = 1;
+    };
+
+    AdjList adjList;
+    int _arcNum;
+
+    //构造无向图
+    ALGraph(const std::vector<VertexType>& _vexs,
+            const std::vector<Arc>& _arcs)
+        : _arcNum(_arcs.size())
+    {
+        adjList.reserve(_vexs.size());
+        for (const auto& vex : _vexs)
+            adjList.emplace_back(vex);
+
+        for (int k = 0; k < _arcs.size(); ++k)
+        {
+            int i = _arcs[k].begin;
+            int j = _arcs[k].end;
+            int weight = _arcs[k].weight;
+
+            ArcNode* arc = new ArcNode();
+            arc->adjvex = j;
+            arc->weight = weight;
+            arc->next = adjList[i].firstArc;
+            adjList[i].firstArc = arc;
+            ++adjList[j].in;
+        }
+    }
+
+    ~ALGraph()
+    {
+        for (int i = 0; i < adjList.size(); ++i)
+        {
+            ArcNode* cur = adjList[i].firstArc;
+            while (cur)
+            {
+                ArcNode* next = cur->next;
+                delete cur;
+                cur = next;
+            }
+        }
+    }
+
+    int vexNum() const { return adjList.size(); }
+    int arcNum() const { return _arcNum; }
+};
+
+
+template <class T>
+bool topologicalSort(ALGraph<T>& graph)
+{
+    std::stack<int> sta;
+    for (int i = 0; i < graph.vexNum(); ++i)
+    {
+        if (graph.adjList[i].in == 0)
+            sta.push(i);
+    }
+    int count = 0;
+    while (!sta.empty())
+    {
+        int index = sta.top();
+        sta.pop();
+        std::cout << graph.adjList[index].data << " -> ";
+        ++count;
+        ArcNode* cur = graph.adjList[index].firstArc;
+        while (cur)
+        {
+            int i = cur->adjvex;
+            if (--graph.adjList[i].in == 0)
+                sta.push(i);
+            cur = cur->next;
+        }
+    }
+    if (count < graph.vexNum())
+        return false;
+    else
+        return true;
+}
+
+
+int main()
+{
+    using namespace std;
+    vector<int> vexs;
+    for (int i = 0; i < 14; ++i) vexs.push_back(i);
+    ALGraph<int> aLGraph(vexs,
+    {
+        {0, 4}, {0, 5}, {0, 11}, {1, 2}, {1, 4},
+        {1, 8}, {2, 5}, {2, 6}, {2, 9}, {3, 2},
+        {3, 13}, {4, 7}, {5, 8}, {5, 12}, {6, 5},
+        {8, 7}, {9, 10}, {9, 11}, {10, 13}, {12, 9}
+    });
+    topologicalSort(aLGraph);
+    cout << endl;
+    return 0;
+}
